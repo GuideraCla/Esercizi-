8e1e9651e5705d77ae77e0c55bb0ea0e042e21e3
GuideraCla <153645391+GuideraCla@users.noreply.github.com>
2023-12-16 22:56:44 +0100

    Add files via upload

---
+++ 3.js
@@ -0,0 +1,9 @@
+const fruits = ["Mela", "Banana", "Kiwi", "Fragola"];
+
+// function logfruit (fruit) {
+//     console.log(fruit)
+// }
+
+// fruits.forEach(logfruit)
+
+fruits.forEach (fruit => console.log(fruit))
