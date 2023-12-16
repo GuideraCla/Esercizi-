491247044e7ac69843ef2a7ed98714e5dfde58e6
GuideraCla <153645391+GuideraCla@users.noreply.github.com>
2023-12-16 22:59:48 +0100

    Add files via upload

---
+++ 7.js
@@ -0,0 +1,7 @@
+// Utilizza una template string per creare una stringa su più righe che includa interruzioni di riga.
+
+const mystring = `Ciao, mi chiamo Claudio, vediamo se effettivamente
+funziona tutto e andiamo a capo correttamente oppure
+devo rifare tutto dall'inizio. `
+
+console.log(mystring)
---
+++ 8.js
@@ -0,0 +1,6 @@
+// Crea una template string che genera un messaggio di saluto con un nome e un messaggio personalizzato.
+const name = "Claudio";
+const message = "come stai?";
+const greeting = `Ciao ${name}! ${message}`;
+
+console.log(greeting);
